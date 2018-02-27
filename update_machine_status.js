d73b672ad70685f053a378debae884835babad6d
karuru6225 <e2hjhi@gmail.com>
2018-02-27 21:22:38 +0900

    場合によっては、SSID/PWが変更されない不具合を修正

--- update_machine_status.js
+++ update_machine_status.js
@@ -134,6 +134,16 @@ co(function*(){
     doUpdate = true;
   }
 
+  if(doUpdate){
+    yield updateMachineStatus(ajax, machine);
+  }
+
+  if(machine.version != gittag){
+    fs.writeFileSync(gittagFile, machine.version);
+    yield log('update version to: ' + machine.version);
+    process.exit(5);
+  }
+
   if(machine.wlanSsid != ssid){
     fs.writeFileSync(ssidFile, machine.wlanSsid);
     yield log('overrided wlanSsid to: ' + machine.wlanSsid);
@@ -144,16 +154,6 @@ co(function*(){
     fs.writeFileSync(pswdFile, machine.wlanPassword);
     yield log('overrided wlanPW to: ' + machine.wlanPassword);
     updatedWlan = true;
-  }
-
-  if(machine.version != gittag){
-    fs.writeFileSync(gittagFile, machine.version);
-    yield log('update version to: ' + machine.version);
-    process.exit(5);
-  }
-
-  if(doUpdate){
-    yield updateMachineStatus(ajax, machine);
   }
 
   if(updatedWlan){
