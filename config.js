f3851eae2991ad70eb9728f996dba055da2b4391
karuru6225 <e2hjhi@gmail.com>
2019-02-25 09:26:23 +0900

    develop設定

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
   url: "http://trial.apppot.net/apppot/",
-  appId: "taisei_kui_staging",
-  appKey: "82ec0045f000469c84b82e062965c73d",
-  appVersion: "1.0.0",
-  // appId: "taisei_kui_develop",
-  // appKey: "b40b0497304642008a9fe808a7bbd9a3",
+  // appId: "taisei_kui_staging",
+  // appKey: "82ec0045f000469c84b82e062965c73d",
   // appVersion: "1.0.0",
+  appId: "taisei_kui_develop",
+  appKey: "b40b0497304642008a9fe808a7bbd9a3",
+  appVersion: "1.0.0",
   companyId: 63
 }
