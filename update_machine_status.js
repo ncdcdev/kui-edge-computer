121f48c217865107f2572cf4f886f0ea69e59ee3
karuru6225 <e2hjhi@gmail.com>
2020-02-21 11:11:16 +0900

    バージョン更新機能　テスト用2

--- update_machine_status.js
+++ update_machine_status.js
@@ -197,7 +197,7 @@ co(function*(){
 
   if(machine.version != gittag){
     fs.writeFileSync(gittagFile, machine.version);
-    console.log('dummy message 1');
+    console.log('dummy message 2');
     yield log('update version to: ' + machine.version);
     process.exit(5);
   }
